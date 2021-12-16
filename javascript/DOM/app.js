67a5c88c955dee96e8d97eb7445f90ec7ecf88b7
BartJanvanRo <hjavanrossum@gmail.com>
2021-12-16 08:48:34 +0100

    donderdag 16 dec

--- javascript/DOM/app.js
+++ javascript/DOM/app.js
@@ -1,18 +1,10 @@
 // window object
 
-let value;
-
-value = window;
-value = window.Number('5')
-
-window.WebDeveloper = (arg)=>{
-    console.log(arg)
-}
 
 /* value = window.WebDeveloper('jack') */
 
-value = window.outerWidth
-value = window.outerHeight
+/* value = window.outerWidth
+value = window.outerHeight */
 
 /*  om de pagina in grootte te laten aanpassen, doet normaal css.
 
@@ -23,7 +15,7 @@ window.addEventListener('resize',(e)=>{
 
 
 
-value = window.document
+/* value = window.document
 
 value = document.body
 
@@ -34,16 +26,17 @@ value = document.location
 
 
 
-value = document.links
+value = document.links */
 
-console.log(value)
 
-value = window.Romeo
+
+/* value = window.Romeo
 //document.location.reload()
 value = document.links[0]
 value = document.links[0].className = "btn btn-danger btn-lg"
 value = document.links[0].getAttribute('key')
 
+console.log(value) */
 /* // forms 
 value = document.forms
 value = document.forms[0]
@@ -54,3 +47,58 @@ value = document.forms[0].method
 
 console.log(value) */
 
+//document object model
+/* console.dir(document); <--alles in het document, dir is natuurlijk directory, zelfs aanpasbaar in terminal*/
+/* console.log(document.domain) -> local host
+ */
+/* console.log(document.URL)  -> local host */ 
+/* console.log(document.title); zo kunnen we vele selecteren*/
+
+/* console.log(document.getElementById('header-title')) */
+
+var headerTitle = document.getElementById('header-title') 
+
+headerTitle.textContent = 'Hello'; // volgt de stijl niet
+headerTitle.innerHTML = 'Goodbye'; // volgt de stijl wel
+headerTitle.innerHTML = '<h2>test</h2>' // stijl wijziging
+
+/* headerTitle.textContent = 'Hello'; // volgt de stijl niet
+headerTitle.innerHTML = 'Goodbye'; // volgt de stijl wel
+headerTitle.innerHTML = '<h2>test</h2>' // stijl wijziging */
+
+
+/* console.log(document.getElementById('header-title')) */
+ 
+//headerTitle.style.border =
+
+/* var items = document.getElementsByClassName('form-group row');
+
+console.log(items);
+console.log(items[1]); */
+
+/* items[1].style.background = gray -> werkt niet, moet via loop
+ */
+ 
+
+
+
+//backgroundcolor aanpassen form
+
+
+ var header = document.querySelector('h1');
+
+ header.style.borderBottom = 'solid 4px #ccc';
+
+ var input = document.querySelector('input');
+input.value = 'Hello world'
+
+
+var itemList = document.querySelector('#header-title');
+
+
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#a8c2e1'
+
+
+itemList = document.querySelector('#first-form');
+console.log(itemList.children);
